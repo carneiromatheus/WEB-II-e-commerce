60da77a3a39c7297f9f22b6db7b0bdd8a308d449
Matheus Carneiro <carneiromatheu@gmail.com>
2025-12-06 21:36:20 -0300

    feat(db): add database connection string for production and development environments

--- src/config/db.connection.js
+++ src/config/db.connection.js
@@ -5,8 +5,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const configDatabase = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString:
+        isProduction ?
+            process.env.DATABASE_PROD_URL :
+            process.env.DATABASE_DEV_URL,
     ssl: true
 };
 
