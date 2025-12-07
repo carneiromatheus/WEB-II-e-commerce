6bc054b2094919f199b5afbb669db0a481886cf9
Matheus Carneiro <carneiromatheu@gmail.com>
2025-12-06 22:17:56 -0300

    refactor(product): updated findProduct function to improve error handling

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -6,22 +6,27 @@ async function findProductById(productId) {
 };
 
 async function findProduct(req, res) {
-    const { id } = req.query;
 
-    if (id) {
-        const productId = parseInt(id, 10);
-        const product = await findProductById(productId);
+    try {
+        const { id } = req.query;
 
-        // FIX: Ajustar retorno quando implementar outros critérios de busca. Retorno único.
-        return res.status(200).send(product);
+        if (id) {
+            const productId = parseInt(id, 10);
+            const product = await findProductById(productId);
+
+            // FIX: Ajustar retorno quando implementar outros critérios de busca. Retorno único.
+            return res.status(200).send(product);
+        };
+
+        // TODO: Caso não tenha id, buscar por outros critérios (nome, categoria, etc);
+        // TODO: Caso não tenha critérios, retornar todos os produtos (com paginação).
+        // FIX: Remover retorno de erro quando implementar os outros critérios.
+
+        res.status(400).send({ message: "Insufficient search parameters." });
+    } catch (error) {
+        res.status(500).send({ message: "Internal server error." });
     };
-
-    // TODO: Caso não tenha id, buscar por outros critérios (nome, categoria, etc);
-    // TODO: Caso não tenha critérios, retornar todos os produtos (com paginação).
-    // FIX: Remover retorno de erro quando implementar os outros critérios.
-
-    res.status(400).send({ message: "Insufficient search parameters." });
-}
+};
 
 export const productController = {
     findProduct
